ef9cf7bba13bd16e7effade4268b4e88ffa4f2a9
Arata Tanaka <tarata43@yahoo.co.jp>
2016-04-19 22:00:59 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='jupyter_gauche',
+      version='0.0.1',
+      description='Gauche Kernel for Jupyter Notebook',
+      author='ara_ta3',
+      author_email='tarata43@yahoo.co.jp',
+      url='http://arata.hatenadiary.com',
+      packages=find_packages(),
+      entry_points="""
+      [console_scripts]
+      """,)
