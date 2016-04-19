5a98b257a5dde63888f693bd3853ac719d19666d
Arata Tanaka <tarata43@yahoo.co.jp>
2016-04-19 22:38:41 +0900

    Add some documents

---
+++ README.md
@@ -0,0 +1,11 @@
+# Jupyter Gauche
+
+A [gauche](http://practical-scheme.net/gauche/) kernel for Jupyter / IPython3
+
+# Install
+
+```
+pip install git+https://github.com/ara-ta3/jupyter-gauche
+python -m jupyter_gauche.install
+```
+
---
+++ info.py
@@ -0,0 +1,12 @@
+INFO = dict(
+            name        = "jupyter_gauche",
+            description = "a gauche kernel for jupyter",
+            author      = "Arata Tanaka",
+            author_email = "tarata43@yahoo.co.jp",
+            license     = "MIT License",
+            url         = "https://github.com/ara-ta3/jupyter-gauche",
+            classifiers = [
+                  "Framework :: IPython",
+                  "License :: OSI Approved :: MIT License"
+                  ]
+)
